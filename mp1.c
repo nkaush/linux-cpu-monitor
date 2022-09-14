fd3cfa9dc69c976d2f302ffcadca638867cde45a
Neil Kaushikkar <neil.kaushikkar@gmail.com>
2022-09-13 19:59:38 -0500

    Implement buggy hello_world module

--- mp1.c
+++ mp1.c
@@ -2,11 +2,48 @@
 
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/list.h>
+#include <linux/timer.h>
+#include <linux/workqueue.h>
+#include <linux/proc_fs.h>
+#include <linux/types.h>
+#include <linux/slab.h>
 #include "mp1_given.h"
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Group_ID");
 MODULE_DESCRIPTION("CS-423 MP1");
+
+#define FILENAME "status"
+#define DIRECTORY "mp1"
+
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+
+static struct proc_dir_entry *proc_dir;
+static struct proc_dir_entry *proc_entry;
+static ssize_t mp1_read(struct file *file, char __user *buffer, size_t count, loff_t *data) {
+   // implementation goes here...
+   int buf_size = count + 1;
+   char* buf = (char *) kmalloc(buf_size, GFP_KERNEL);
+   int copied = 0;
+
+   snprintf(buf, MIN(buf_size, 6), "%s", "hello");
+   copied += 6;
+
+   copy_to_user(buffer, buf, copied);
+   kfree(buf);
+   return copied;
+}
+
+static ssize_t mp1_write(struct file *file, const char __user *buffer, size_t count, loff_t *data) {
+   // implementation goes here...
+   return 0;
+}
+
+static const struct proc_ops mp1_file = {
+   .proc_read = mp1_read,
+   .proc_write = mp1_write,
+};
 
 #define DEBUG 1
 
@@ -18,6 +55,8 @@ int __init mp1_init(void)
    #endif
    // Insert your code here ...
    
+   proc_dir = proc_mkdir(DIRECTORY, NULL);
+   proc_entry = proc_create(FILENAME, 0666, proc_dir, &mp1_file);
    
    
    printk(KERN_ALERT "MP1 MODULE LOADED\n");
@@ -32,7 +71,8 @@ void __exit mp1_exit(void)
    #endif
    // Insert your code here ...
    
-   
+   remove_proc_entry(FILENAME, proc_dir);
+   remove_proc_entry(DIRECTORY, NULL);
 
    printk(KERN_ALERT "MP1 MODULE UNLOADED\n");
 }
